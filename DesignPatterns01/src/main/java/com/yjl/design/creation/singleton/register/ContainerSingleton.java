05cf50acf9107cd465f8f805e2db948522311133
yujiale <yu18371919563@163.com>
2022-01-24 14:22:12 +0800

    单例模式之注册单例（枚举）

--- DesignPatterns01/src/main/java/com/yjl/design/creation/singleton/register/ContainerSingleton.java
+++ DesignPatterns01/src/main/java/com/yjl/design/creation/singleton/register/ContainerSingleton.java
@@ -1,6 +1,32 @@
-package com.yjl.design.creation.singleton.register;/**
-*@program: DesignPatterns
-*@author: yjl
-*@created: 2022/01/24
-*/public class ContainerSingleton {
+package com.yjl.design.creation.singleton.register;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @program: DesignPatterns
+ * @author: yjl
+ * @created: 2022/01/24
+ */
+public class ContainerSingleton {
+
+
+    private ContainerSingleton(){}
+
+    private static Map<String,Object> ioc = new ConcurrentHashMap<String, Object>();
+
+    public static Object getInstance(String className){
+        Object instance = null;
+        if(!ioc.containsKey(className)){
+            try {
+                instance = Class.forName(className).newInstance();
+                ioc.put(className, instance);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return instance;
+        }else{
+            return ioc.get(className);
+        }
+    }
 }
--- DesignPatterns01/src/main/java/com/yjl/design/creation/singleton/register/EnumSingleton.java
+++ DesignPatterns01/src/main/java/com/yjl/design/creation/singleton/register/EnumSingleton.java
@@ -1,6 +1,23 @@
-package com.yjl.design.creation.singleton.register;/**
-*@program: DesignPatterns
-*@author: yjl
-*@created: 2022/01/24
-*/public enum EnumSingleton {
+package com.yjl.design.creation.singleton.register;
+
+/**
+ * @program: DesignPatterns
+ * @author: yjl
+ * @created: 2022/01/24
+ */
+public enum EnumSingleton {
+
+    INSTANCE;
+
+    private Object data;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public static EnumSingleton getInstance(){return INSTANCE;}
 }
