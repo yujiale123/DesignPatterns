2f56d713b011fe26ebf86d364eabcd7227efb418
yujiale <yu18371919563@163.com>
2022-01-24 10:05:25 +0800

    单例模式之饿汉式1

--- DesignPatterns01/src/main/java/com/yjl/design/creation/singleton/hungry/StaticSingleton.java
+++ DesignPatterns01/src/main/java/com/yjl/design/creation/singleton/hungry/StaticSingleton.java
@@ -6,6 +6,10 @@ package com.yjl.design.creation.singleton.hungry;
  * @created: 2022/01/24
  */
 public class StaticSingleton {
+    //先静态后动态
+    //先上后下
+    //先属性后方法
+
 
     private static final StaticSingleton singleton;
 
