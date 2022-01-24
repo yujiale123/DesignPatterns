31acb327be0fee5c63e82e4f4f63dc43e0c79418
yujiale <yu18371919563@163.com>
2022-01-24 15:21:31 +0800

    单例模式之序列化

--- DesignPatterns01/src/main/java/com/yjl/design/creation/singleton/seriable/SerializableSingletonTest.java
+++ DesignPatterns01/src/main/java/com/yjl/design/creation/singleton/seriable/SerializableSingletonTest.java
@@ -1,9 +1,41 @@
 package com.yjl.design.creation.singleton.seriable;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * @program: DesignPatterns
  * @author: yjl
  * @created: 2022/01/24
  */
-public class SeriableSingletonTest {
+public class SerializableSingletonTest {
+    public static void main(String[] args) {
+
+        SerializableSingleton s1 = null;
+        SerializableSingleton s2 = SerializableSingleton.getInstance();
+
+        FileOutputStream fos = null;
+        try {
+
+            fos = new FileOutputStream("SeriableSingleton.obj");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(s2);
+            oos.flush();
+            oos.close();
+
+            FileInputStream fis = new FileInputStream("SeriableSingleton.obj");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            s1 = (SerializableSingleton)ois.readObject();
+            ois.close();
+
+            System.out.println(s1);
+            System.out.println(s2);
+            System.out.println(s1 == s2);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
